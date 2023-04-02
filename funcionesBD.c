0834761c9b313878dc20e78c322b946b8d3834a4
DJLamentablisimo <81747616+DJLamentablisimo@users.noreply.github.com>
2023-04-02 17:58:20 +0200

    funcionalidad basica bbdd

--- funcionesBD.c
+++ funcionesBD.c
@@ -162,5 +162,31 @@ int crearTablas(sqlite3* db) {
 	return SQLITE_OK;
 }
 
+User getUsuario(char* nombre, char* contrasena, sqlite3* db){
+	sqlite3_stmt *stmt;
+    User usu;
+	char seq[100];
+	sprintf(seq, "SELECT * FROM USUARIO WHERE NOM_USER = '%s' AND PASSWORD_USER = '%s'", nombre, encrypt(contrasena));
 
+	if (sqlite3_prepare_v2(db, seq, -1, &stmt, NULL) != SQLITE_OK) {
+		printf("Error al cargar el usuario\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return (User){'\0', 0, 0};
+	}
+	int i =sqlite3_step(stmt);
+	if(i != SQLITE_ROW){
+		return (User){'\0', 0, 0};
+	}
+	usu.id_User = sqlite3_column_int(stmt, 0);
+	strcpy(usu.nom_User, (char *) sqlite3_column_text(stmt, 1));
+	strcpy(usu.password, (char *) sqlite3_column_text(stmt, 2));
+	usu.tipo = sqlite3_column_int(stmt, 3);
+	return usu;
+}
+
+void addUsuario(char* nombre, char* contrasena, int admin, sqlite3* db){
+	char seq[200];
+	sprintf(seq, "INSERT INTO USUARIO(ID_USER, NOM_USER, PASSWORD_USER, TIPO_USER) VALUES (%i, '%s', '%s', %d)",getUsuarioCount(), nombre, encrypt(contrasena), admin);
+	update(seq);
+}
 
