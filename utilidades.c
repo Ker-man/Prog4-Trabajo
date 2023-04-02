a1d5b18146732eff18474b4460c67488cf717a21
Andoni Torres <71408357+Trufatrula@users.noreply.github.com>
2023-04-02 18:38:58 +0200

    Utilidades para menus

---
+++ utilidades.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "utilidades.h"
+
+/*
+int main(void) {
+    const char *opciones[] = {"Si", "No", "Tal vez"};
+    int o = opcion("Prueba?", 3, opciones);
+    return 0;
+}
+*/
+
+int opcion(const char *pregunta, int num_opciones, const char **opciones)
+{
+    printf("%s:\n", pregunta);
+    for (int i = 0; i < num_opciones; i++)
+    {
+        printf(" %i. %s\n", i + 1, opciones[i]);
+    }
+    int eleccion = 0;
+    do
+    {
+        printf("Opcion: (1 - %i): ", num_opciones);
+        char linea[20];
+        fgets(linea, 20, stdin);
+        sscanf(linea, "%i", &eleccion);
+    } while (eleccion < 1 || eleccion > num_opciones);
+    return eleccion - 1;
+    }
---
+++ utilidades.h
@@ -0,0 +1,6 @@
+#ifndef _UTILIDADES_H_
+#define _UTILIDADES_H_
+
+int opcion(const char* pregunta, int num_opciones, const char** opciones);
+
+#endif
