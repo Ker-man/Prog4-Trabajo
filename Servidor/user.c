a5523afe0f1559b9ed2090658c85a4caeac38194
Ker-man <kerman.urrutikoetxea@opendeusto.es>
2023-05-23 03:59:21 +0200

    USER con su funcinalidad
    Mover de funciones a User

--- Servidor/user.c
+++ Servidor/user.c
@@ -39,3 +39,81 @@ void leerCopiaSeguridad(){
     printf("\n");
     fclose(f);
 }
+
+
+int getUsuarioCount(sqlite3* db){
+	sqlite3_stmt *stmt;
+		if (sqlite3_prepare_v2(db, "SELECT COUNT(*) FROM USUARIO", -1, &stmt, NULL) != SQLITE_OK) {
+			printf("Error al cargar el usuario\n");
+			printf("%s\n", sqlite3_errmsg(db));
+			return 0;
+		}
+		int i =sqlite3_step(stmt);
+		if(i != SQLITE_ROW){
+			printf("Aún no hay datos generados\n");
+			return 0;
+		}
+		return sqlite3_column_int(stmt, 0);
+}
+
+User* getAllUsers(sqlite3* db){
+	User* allUsers = (User*)malloc(sizeof(User)* getUsuarioCount(db));
+	for (int i = 0; i<getUsuarioCount(db); i++){
+		allUsers[i] = getUsuarioFromID(i, db);
+	}
+	return allUsers;
+}
+User getUsuario(char* nombre, sqlite3* db){
+	sqlite3_stmt *stmt;
+    User usu;
+	char seq[100];
+	sprintf(seq, "SELECT * FROM USUARIO WHERE NOM_USER = '%s'", nombre);
+
+	if (sqlite3_prepare_v2(db, seq, -1, &stmt, NULL) != SQLITE_OK) {
+		printf("Error al cargar el usuario\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return (User){0,'\0', 0, 0};
+	}
+	int i = sqlite3_step(stmt);
+	if(i != SQLITE_ROW){
+		printf("\nEste usuario no se encuentra registrado\n");
+		return (User){-1,"USER_ERROR", "ERROR"};
+	}
+	usu.id_User = sqlite3_column_int(stmt, 0);
+	strcpy(usu.nom_User, (char *) sqlite3_column_text(stmt, 1));
+	strcpy(usu.password, (char *) sqlite3_column_text(stmt, 2));
+	usu.tipo = sqlite3_column_int(stmt, 3);
+	return usu;
+}
+
+User getUsuarioFromID(int id, sqlite3* db){
+	sqlite3_stmt *stmt;
+    User usu;
+	char seq[100];
+	sprintf(seq, "SELECT * FROM USUARIO WHERE ID = %i", id);
+
+	if (sqlite3_prepare_v2(db, seq, -1, &stmt, NULL) != SQLITE_OK) {
+		printf("Error al cargar el usuario\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return (User){'\0', 0, 0};
+	}
+	int i =sqlite3_step(stmt);
+	if(i != SQLITE_ROW){
+		return (User){'\0', 0, 0};
+	}
+	usu.id_User = sqlite3_column_int(stmt, 0);
+	strcpy(usu.nom_User, (char *) sqlite3_column_text(stmt, 1));
+	strcpy(usu.password, (char *) sqlite3_column_text(stmt, 2));
+	usu.tipo = sqlite3_column_int(stmt, 3);
+	return usu;
+}
+
+void addUsuario(char* nombre, char* contrasena, int admin, sqlite3* db){
+	int cont = getUsuarioCount(db);
+	char seq[200];
+	sprintf(seq, "INSERT INTO USUARIO(ID, NOM_USER, PASSWORD_USER, TIPO_USER) VALUES (%i, '%s', '%s', %i)",cont+1, nombre, contrasena, admin);
+	update(seq, db);
+	escribirCopiaSeguridad(cont+1, nombre, contrasena, admin);
+
+	printf("Usuario creado correctamente, pulse cualquier tecla para continuar\n");
+}
--- Servidor/user.h
+++ Servidor/user.h
@@ -1,5 +1,6 @@
 #ifndef _USER_H_
 #define _USER_H_
+#include "sqlite3.h"
 
 typedef struct 
 {
@@ -12,5 +13,13 @@ typedef struct
 void imprimirUser(User user);
 void escribirCopiaSeguridad(int id,char* nombre, char* contrasena, int admin);
 void leerCopiaSeguridad();
+
+
+int getUsuarioCount(sqlite3* db);
+User* getAllUsers(sqlite3* db);
+User getUsuario(char* nombre, sqlite3* db);
+User getUsuarioFromID(int id, sqlite3* db);
+void addUsuario(char* nombre, char* contrasena, int admin, sqlite3* db);
+
 
 #endif
